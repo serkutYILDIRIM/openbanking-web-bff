a1df419aea57902186ddeb76b4bc81ec0b7fc619
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-03-01 00:04:48 +0300

    initial commit

---
+++ src/main/java/com/openbanking/webbff/OpenbankingWebBffApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.webbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingWebBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankingWebBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openbanking/webbff/OpenbankingWebBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbanking.webbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankingWebBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
