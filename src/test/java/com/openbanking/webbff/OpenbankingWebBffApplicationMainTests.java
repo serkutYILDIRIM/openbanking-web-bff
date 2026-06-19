e2e03d79d5822fcf308ad0d08a5a562736beb014
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-06-19 23:57:07 +0300

    Update OpenbankingWebBffApplicationMainTests.java

--- src/test/java/com/openbanking/webbff/OpenbankingWebBffApplicationMainTests.java
+++ src/test/java/com/openbanking/webbff/OpenbankingWebBffApplicationMainTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OpenbankingWebBffApplicationMainTests {
-
     @Test
     void mainMethodRunsSuccessfully() {
         OpenbankingWebBffApplication.main(new String[]{});
